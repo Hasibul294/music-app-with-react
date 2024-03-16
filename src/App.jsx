207b1125720a19f6bf63b40cfd726e0b5c49736a
Hasibul294 <hasibulhasan294@gmail.com>
2024-03-17 04:04:38 +0600

    initilize project and tailwind css for design

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-slate-900 w-screen h-screen">
+        <h1 className="text-5xl font-bold text-white">
+          This is my music app with vite.{" "}
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

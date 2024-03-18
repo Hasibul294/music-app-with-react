1345abecd33e2e4797f1e4daa42544b1be73d21b
Hasibul294 <hasibulhasan294@gmail.com>
2024-03-19 04:05:10 +0600

    add top song compo and my player component

---
+++ src/components/ui/PlayPause.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={40} className="text-blue-400" onClick={handlePause} />
+  ) : (
+    <FaPlayCircle size={40} className="text-blue-400" onClick={handlePlay} />
+  );
+
+export default PlayPause;

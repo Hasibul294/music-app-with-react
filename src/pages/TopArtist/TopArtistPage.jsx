389a571843f768a99e7c83c5bbc58bdc730d05d8
Hasibul294 <hasibulhasan294@gmail.com>
2024-03-19 23:46:57 +0600

    final push

--- src/pages/TopArtist/TopArtistPage.jsx
+++ src/pages/TopArtist/TopArtistPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const TopArtistPage = () => {
-  return <div>This Top Artist page</div>;
+  return (
+    <div>
+      <p className="text-white text-center text-2xl mt-10">
+        This is page is coming soon,,,
+      </p>
+    </div>
+  );
 };
 
 export default TopArtistPage;
